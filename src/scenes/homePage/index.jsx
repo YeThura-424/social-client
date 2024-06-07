1a9ae6e395e29d6fa8ef283433336234eb10e1e4
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-07 21:23:11 +0630

    navbar

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,11 +1,13 @@
+import { Box } from "@mui/material";
 import { Fragment } from "react";
+import Navbar from "scenes/navbar";
 
 const HomePage = () => {
   return (
     <Fragment>
-      <div>
-        <h1>Welcome To Home Page </h1>
-      </div>
+      <Box>
+        <Navbar />
+      </Box>
     </Fragment>
   )
 }
